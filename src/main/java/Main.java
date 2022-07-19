8cde039cd260a792971c1f48dc8787085c97e077
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-19 12:06:05 -0400

    Finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
 import java.util.function.UnaryOperator;
 
+/*
+ * Problem:
+ * x Fix the modifiers of the PREFIX and SUFFIX variables so they can be used in the operator lambda expression.
+ * x  Fix the lambda expression so that:
+ *      x str doesn’t have any whitespace.
+ *      x The PREFIX and SUFFIX values are added to the string.
+ */
+
 public class Main {
-    String PREFIX = "__prefix__";
-    String SUFFIX = "__suffix__";
-	
-	// fix the operator code
-    public static UnaryOperator<String> operator = str -> str;
+    public static final String PREFIX = "__prefix__";
+    public static final String SUFFIX = "__suffix__";
+
+    // fix the operator code
+    public static UnaryOperator<String> operator = str -> PREFIX + str.trim() + SUFFIX;
 
     public static void main(String[] args) {
-        String res = operator.apply("  hello"); 
+        String res = operator.apply("  hello");
         System.out.println(res); // __prefix__hello__suffix__
     }
 }
